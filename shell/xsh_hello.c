f50ed736b7b21a936f75e1473afcb8ef741e226c
ranagara <ranagara@iu.edu>
2016-09-19 17:50:13 -0400

    first commit in new branch

---
+++ shell/xsh_hello.c
@@ -0,0 +1,36 @@
+/* xsh_hello.c - xsh_hello*/
+
+#include <xinu.h>
+#include <string.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xsh_hello - print the string argument passed to it
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_hello(int32 nargs, char *args[])
+{
+
+char welcome_string[45] = "Welcome to the world of Xinu!!";
+
+if(nargs == 2) 
+{
+	//print the <string>, Welcome to the world of Xinu!!
+	
+	printf("Hello %s, %s\n",args[1],welcome_string);
+	return 0;
+}
+
+if(nargs > 2)
+{
+	fprintf(stderr,"%s: too many arguments passed\n",args[0]);
+	return 1;
+}
+
+if(nargs <2)
+{
+	fprintf(stderr, "Too few arguments!\n");
+	return 1;
+}
+return 0;
+}
