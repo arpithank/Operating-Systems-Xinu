42e21b848ca0f20282ee76635f6c0abd7060d5a1
ranagara <ranagara@iu.edu>
2016-09-20 18:05:56 -0400

    atoi added

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -1,14 +1,17 @@
 #include <xinu.h>
 #include <prodcons.h>
-
+#include <stdio.h>
 int global_var;
 
 shellcmd xsh_prodcons(int32 narg, char* args[])
 {
 	int val = 2000;
 	
-	if(narg ==2 && args[1] != NULL)
-		val = args[1];
+	if(narg == 2 && args[1] != NULL)
+	{
+		val = atoi(args[1]);
+		printf("value is %d",val);
+	}
 
 	if(narg > 2)
 	{
@@ -16,7 +19,7 @@ shellcmd xsh_prodcons(int32 narg, char* args[])
 		return 1;
 	}
 	
-	if(narg <2)
+	if(narg < 2)
 	{
 		fprintf(stderr, "\n Too few arguments!");
 		return 1;
