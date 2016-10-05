e822d4cbd1aca272db7ea39ad36be6517ed87294
ranagara <ranagara@iu.edu>
2016-10-05 18:07:03 -0400

    future modificatons

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -2,15 +2,14 @@
 #include <prodcons.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <future.h>
 int n;
 sid32 produced, consumed;
 
 shellcmd xsh_prodcons(int32 narg, char* args[])
 {
 	int count = 2000;
-	
-	//printf("\nargs[1] %s",args[1]);
-	//printf("\nnumber of arg %d",narg);
+	future *f1, *f2, *f3;
 
 	if(narg > 2)
 	{
@@ -31,7 +30,16 @@ shellcmd xsh_prodcons(int32 narg, char* args[])
 	produced = semcreate(0);
 	consumed = semcreate(1);
 
-
+	f1 = future_alloc(FUTURE_EXCLUSIVE);
+	f2 = future_alloc(FUTURE_EXCLUSIVE);
+	f3 = future_alloc(FUTURE_EXCLUSIVE);
+	       
+	resume( create(future_cons, 1024, 20, "fcons1", 1, f1);
+	resume( create(future_prod, 1024, 20, "fprod1", 1, f1) );
+	resume( create(future_cons, 1024, 20, "fcons2", 1, f2) );
+	resume( create(future_prod, 1024, 20, "fprod2", 1, f2) );
+	resume( create(future_cons, 1024, 20, "fcons3", 1, f3) );
+	resume( create(future_prod, 1024, 20, "fprod3", 1, f3) );
 	resume(create(producer,1024,20,"producer",1,count));
 	resume(create(consumer,1024,20,"consumer",1,count));
 	
