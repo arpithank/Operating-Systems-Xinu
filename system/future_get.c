f94aca1a7492b10a575db494ecad1d0110e94055
ranagara <ranagara@iu.edu>
2016-10-06 16:42:58 -0400

    changes for assignment4

--- system/future_get.c
+++ system/future_get.c
@@ -7,8 +7,9 @@ syscall future_get(future* f,int* value)
 //	*value = *f->value;
 	if(f->state == FUTURE_VALID)
 	{
+		*value = *f->value;
 		f->state = FUTURE_EMPTY;
-		return SYSERR;
+		return OK;
 	}
 
 	if(f->state == FUTURE_EMPTY)
@@ -17,12 +18,14 @@ syscall future_get(future* f,int* value)
 		*value = *f->value;
 		f->pid = getpid();
 		suspend(getpid());
+		*value = *f->value;
+		f->state = FUTURE_EMPTY;
 		return OK;
 	}
 
 	if(f->state == FUTURE_WAITING)
 	{
-		f->state = FUTURE_VALID;
-		return OK;
+		f->state = FUTURE_EMPTY;
+		return SYSERR;
 	}
 }
