e86bf4318215cef730e4e8816be56257ecafef88
ranagara <ranagara@iu.edu>
2016-10-06 14:48:34 -0400

    removed extra prints

--- apps/future_prod.c
+++ apps/future_prod.c
@@ -6,18 +6,16 @@ uint future_prod(future *fut)
 {
 	int i, j;
 	j = (int)fut;
-	printf("j = %d",j);
+	//printf("j = %d",j);
 	for (i=0; i<10; i++) 
 	{
 		j+=i;
-		printf("\n Value produced %d",j);
-		printf("\n Calling set \n");
+		printf("\n Value produced %d\n",j);
+		//printf("\n Calling set \n");
 		future_set(fut, &j);
 
 	}
-		//calling set with the produced value
-		//printf("\n Calling set \n");
-	        //future_set(fut, &j);
+		
 		return OK;
 }
 
